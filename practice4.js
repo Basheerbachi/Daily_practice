c42ffef0133e4a671571abd2a24e4db4b20f3645
Basheerbachi <85440600+Basheerbachi@users.noreply.github.com>
2022-03-30 17:57:58 +0530

    Add files via upload

---
+++ practice4.js
@@ -0,0 +1,32 @@
+// let user = {
+//     'name' : 'Basheer',
+//     age : 25,
+//     isAdmin : false,
+//     fruit : apple,
+// }
+// console.log(user.name);
+// console.log(user['name']);
+
+// let salaries = {
+//     john : 100,
+//     kevin : 160
+// }
+
+
+// for(let key in salaries){
+//     let sum += (salaries[key])
+//     console.log([sum]);
+// }
+
+// let menu = {
+//     width : 200,
+//     height : 400,
+//     title : 'my menu'
+// }
+
+// for(let i in menu){
+//     if(typeof menu[i] === 'number'){
+//         menu[i] = menu[i] * 2
+//     }
+// }
+// console.log(menu);
