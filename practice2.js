110a0d8cd3a042fb4d92afb5f55ae342f0ad77db
Basheerbachi <85440600+Basheerbachi@users.noreply.github.com>
2022-03-30 18:02:15 +0530

    Add files via upload

---
+++ practice2.js
@@ -0,0 +1,53 @@
+// let fruits = ['apple', 'banana', 'orange', 'kiwi']
+
+// // fruits.pop();
+// // console.log(fruits)
+// // console.log(fruits.pop())
+// // console.log(fruits)
+// // console.log(fruits.push('strawberry'))
+// // console.log(fruits.push('melon'))
+// // console.log(fruits)
+
+// //shift and unshift are similar to push and pop but they works on 1st array element
+// // let array = [10,20,30,40,50]
+// // let result = console.log(array.unshift(5));
+// // console.log();
+
+
+
+// //-------indexof- returns the first index at which a given element occurs
+// //it has second argument, which specifies at which index you want to start the start
+// // let fruits = ['apple', 'banana', 'orange', 'kiwi','banana','jackfruit','cherry','cheeku','banana']
+// // let res=fruits.indexOf('banana', 5)
+// // console.log(res);
+
+// //-------slice - 
+// // let fruits = ['apple', 'banana', 'orange', 'kiwi','melon','grapes']
+// // let result = fruits.slice(1,5)  // 
+// // console.log(result)                                       // ['banana', 'orange', 'kiwi', 'melon']
+// // let result = fruits.slice(2)  // 
+// // console.log(result)                                       // 
+// //slice works with negative index as well
+// //              -6       -5        -4       -3     -2       -1     
+// // fruits = ['apple', 'banana', 'orange', 'kiwi','melon','grapes']
+// // let result = fruits.slice(-4,5)  
+// // console.log(result)                                     // ['orange', 'kiwi', 'melon']
+
+// //practice
+// let fruits = ['apple', 'banana', 'orange', 'kiwi', 'melon', 'tomato', 'grapes']
+
+// console.log(fruits.push('strawberry', 'papaya', 'pear'))
+// console.log(fruits.pop())
+// console.log(fruits.shift())
+// console.log(fruits.unshift('jackfruit'))
+// let result = fruits.slice(0, 3)
+// console.log(result)
+
+
+// 10
+// pear 
+// apple
+// 9
+// 'jackfruit','banana', 'orange'
+// 'jackfruit','banana', 'orange', 'kiwi', 'melon', 'tomato', 'grapes','strawberry', 'papaya'
+// //'jackfruit','banana', 'orange', 'kiwi', 'melon', 'tomato', 'grapes','strawberry', 'papaya'
