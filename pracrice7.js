dc3531c41e0c803b6ad1aea49950d23ffa0d02c4
Basheerbachi <85440600+Basheerbachi@users.noreply.github.com>
2022-03-30 17:50:13 +0530

    Add files via upload

---
+++ pracrice7.js
@@ -0,0 +1,46 @@
+// let arr = [1, 2, 3, 4]
+
+// function copyArrayMultiplyBy2(array) {
+//     const output = []
+//     for (let i = 0; i < array.length; i++) {
+//         output.push(array[i] * 2)
+//     }
+//     return output
+// }
+
+// console.log(copyArrayMultiplyBy2(arr))
+
+// copyArrayAdd2(arr)
+
+// function copyArrayMultiplyBy2(array) {
+//     const output = []
+//     for (let i = 0; i < array.length; i++) {
+//         output.push(array[i] + 2)
+//     }
+//     return output
+// }
+
+// Dry principle is broken
+
+
+
+
+// let arr = [1, 2, 3, 4]
+
+// function copyArrayAndManipulate(array, instructions) {
+//     let output = []
+
+//     for (let i = 0; i < array.length; i++) {
+//         output.push(instructions(array[i]))
+//     }
+
+//     return output
+// }
+
+// //instructions
+// function multiplyBy2(input) {
+//     return input * 2
+// }
+// function add2(input) {
+//     return input + 2
+// }
