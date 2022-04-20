ee41dd3f97fb4374f37424aa5a8f78ecbedadfcc
Basheerbachi <85440600+Basheerbachi@users.noreply.github.com>
2022-04-20 14:21:48 +0530

    Add files via upload

---
+++ practice15.js
@@ -0,0 +1,47 @@
+//js is functional programing language
+//-----JQuery
+
+// $(".heading").click(function () {
+//   $("p").hide();
+// });
+
+// $("#heading2").dblclick(function () {
+//   $("p").hide();
+// });
+
+// $("h2").dblclick(function () {
+//   $("span").hide();
+// });
+
+// $("#heading2").dblclick(function () {
+//   $(this).hide();
+// });
+
+//---------Ajax
+//Is a function which takes an object of diff keys
+// $("button").click(function(){
+//     $.ajax({
+//       url:"https://jsonplaceholder.typicode.com/todos",
+//       type:"GET",
+//       success:function(data)
+//       {
+//         console.log(data);
+//       }
+//     })
+// })
+// //CRUD:-Create,read,update,delete
+// //GET POST PUT DELETE
+
+// class A {
+//  constructor(name) {
+//    this.username = name;
+//  }
+//  print() {
+//    console.log("Name is :", this.username);
+//  }
+// }
+// var a = new A("Tom");
+// a.print();
+// console.log(a.username);
+
+// class B extends A {}
